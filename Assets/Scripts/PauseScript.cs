dfdea2495193a3bbad0ce334efe7089d74b7acc8
FrancisShehata <83419373+FrancisShehata@users.noreply.github.com>
2022-04-07 19:13:08 -0400

    Added pause menu
    added pause menu to final project

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+
+{
+    // C = is pause menu
+    // S = status menu
+    [SerializeField]
+    Canvas c, s;
+
+
+
+    public bool isPaused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+             
+            }
+            else
+            {
+                Pause();
+                  
+            }
+        }
+    }
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        isPaused = true;
+        c.enabled = true;
+        s.enabled = false;
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+        c.enabled = false;
+        s.enabled = true;
+    }
+}
